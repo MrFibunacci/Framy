8049e435b5e9502bc13188db239c59722d62446d
Unknown <mrfibunacci@gmail.com>
2017-07-13 12:37:54 +0000

    first version to be tested
    Basic Model 1.1

--- app/framework/Component/http/Model/Model.php
+++ app/framework/Component/http/Model/Model.php
@@ -7,4 +7,57 @@
     class Model //implements \ArrayAccess, \JsonSerializable
     {
         use StorageTrait,StdLibTrait;
+
+        /**
+         * The table associated with the model.
+         *
+         * @var string
+         */
+        protected $table;
+
+        /**
+         * The connection name for the model.
+         *
+         * @var string
+         */
+        protected $connection;
+
+        /**
+         * Contains the Database instance
+         *
+         * @var \app\framework\Component\Database\Medoo
+         */
+        private $DB;
+
+        function __consturct()
+        {
+            if($this->is($connection)){
+                // setup connection
+                $this->DB = new app\framework\Component\Database\Meodo($this->connection);
+            }
+        }
+
+        protected function DB($dbname = null){
+            if($this->is($dbname)){
+                $this->DB = new \app\framework\Component\Database\Medoo($this->connenction);
+            } else {
+                $this->DB = new \app\framework\Component\Database\Medoo($dbname);
+            }
+
+            return $this;
+        }
+
+        protected function select($join, $columns = null, $where = null, $table = null)
+        {
+            //TODO: you may have to rewrite every single Core SQL function to achive the "Easy DB Accsess"
+            if(!$this->is($table) && $this->is($this->table){
+                return $this->DB->select($this->$table, $join, $columns, $where);
+            } elseif($this->is($table)){
+                return $this->DB->select($table, $join, $columns, $where);
+            } else {
+                throw new \Exception("You need to specify an table");
+            }
+        }
     }
+
+    //$this->DB()->select()
