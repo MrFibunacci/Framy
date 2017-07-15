80b1ab67233679fb8c3a5ca15b866d4758f0c12d
MrFibunacci <mrfibunacci@gmail.com>
2017-07-15 16:01:37 +0200

    First "Easy Database Access" version only with "select" function for now. I'll add later more.

--- app/framework/Component/http/Model/Model.php
+++ app/framework/Component/http/Model/Model.php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * Framey Framework
+ *
+ * @copyright Copyright Framey
+ * @Author Marco Bier <mrfibunacci@gmail.com>
+ */
+
     namespace app\framework\Component\http\Model;
 
+    use app\framework\Component\StdLib\StdLibTrait;
     use app\framework\Component\Storage\StorageTrait;
-    use app\framework\Component\TemplateEngine\TemplateEngine;
+    use app\framework\Component\Database\Medoo as Database;
 
+
+    /**
+     * <code>Basic Model 1.1
+     *   with Easy Database Access 1.2
+     * </code>
+     *
+     * @package app\framework\Component\http\Model
+     */
     class Model //implements \ArrayAccess, \JsonSerializable
     {
         use StorageTrait,StdLibTrait;
@@ -25,24 +41,24 @@
         /**
          * Contains the Database instance
          *
-         * @var \app\framework\Component\Database\Medoo
+         * @var \app\framework\Component\Database\Medoo|null
          */
         private $DB;
 
-        function __consturct()
+        function __construct()
         {
-            if($this->is($connection)){
+            if($this->is($this->connection)){
                 // setup connection
-                $this->DB = new app\framework\Component\Database\Meodo($this->connection);
+                $this->DB = new Database($this->connection);
             }
         }
 
-        protected function DB($dbname = null){
-            if((!$this->instanceOf($this->DB, "\app\framework\Component\Database\Medoo") && !$this->is($connection)) && !$this->is($dbname)){
-                if($this->is($dbname)){
-                    $this->DB = new \app\framework\Component\Database\Medoo($this->connenction);
+        protected function DB($dbName = null){
+            if(($this->isInstanceOf($this->DB, "\\app\\framework\\Component\\Database\\Medoo") && $this->is($this->connection)) && $this->isNull($dbName)){
+                if($this->isNull($dbName)){
+                    $this->DB = new Database($this->connection);
                 } else {
-                    $this->DB = new \app\framework\Component\Database\Medoo($dbname);
+                    $this->DB = new Database($dbName);
                 }
                 return $this;
             } else {
@@ -50,17 +66,14 @@
             }
         }
 
-        protected function select($join, $columns = null, $where = null, $table = null)
+        protected function select($columns = null, $where = null, $table = null)
         {
-            //TODO: you may have to rewrite every single Core SQL function to achive the "Easy DB Accsess"
-            if(!$this->is($table) && $this->is($this->table){
-                return $this->DB->select($this->$table, $join, $columns, $where);
+            if($this->isNull($table) && $this->is($this->table)){
+                return $this->DB->select($this->table, $columns, $where);
             } elseif($this->is($table)){
-                return $this->DB->select($table, $join, $columns, $where);
+                return $this->DB->select($table, $columns, $where);
             } else {
                 throw new \Exception("You need to specify an table");
             }
         }
     }
-
-    //$this->DB()->select()
